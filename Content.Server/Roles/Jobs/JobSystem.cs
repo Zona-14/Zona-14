4d9281d436ec14f202c00f32921d4a29a869e23c
QEWAEWA <ewseriws@mail.ru>
2026-03-20 19:17:44 +0300

    .

--- Content.Server/Roles/Jobs/JobSystem.cs
+++ Content.Server/Roles/Jobs/JobSystem.cs
@@ -1,11 +1,6 @@
-﻿using System.Globalization;
+using System.Globalization;
 using Content.Server.Chat.Managers;
-// stalker-changes
-using Content.Shared.Actions;
-// stalker-changes-end
 using Content.Shared.Mind;
-using Content.Server.Mind;
-using Content.Server.Roles;
 using Content.Shared.Roles;
 using Content.Shared.Roles.Jobs;
 using Robust.Shared.Player;
@@ -19,27 +14,18 @@ public sealed class JobSystem : SharedJobSystem
 {
     [Dependency] private readonly IChatManager _chat = default!;
     [Dependency] private readonly ISharedPlayerManager _player = default!;
-    [Dependency] private readonly MindSystem _minds = default!;
     [Dependency] private readonly RoleSystem _roles = default!;
-    [Dependency] private readonly SharedActionsSystem _actions = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<RoleAddedEvent>(OnRoleAddedEvent);
         SubscribeLocalEvent<RoleRemovedEvent>(OnRoleRemovedEvent);
-        SubscribeLocalEvent<ShowJobRulesActionEvent>(OnShowJobRulesAction);
     }
 
     private void OnRoleAddedEvent(RoleAddedEvent args)
     {
         MindOnDoGreeting(args.MindId, args.Mind, args);
-
-        if (args.Mind.OwnedEntity is { } ownedEntity && MindTryGetJob(args.MindId, out var prototype))
-        {
-            ShowJobRules(ownedEntity, args.Silent, prototype);
-            AddJobRulesAction(ownedEntity, prototype);
-        }
 
         if (args.RoleTypeUpdate)
             _roles.RoleUpdateMessage(args.Mind);
@@ -47,9 +33,6 @@ public sealed class JobSystem : SharedJobSystem
 
     private void OnRoleRemovedEvent(RoleRemovedEvent args)
     {
-        if (args.Mind.OwnedEntity is { } ownedEntity)
-            RemoveJobRulesAction(ownedEntity);
-
         if (args.RoleTypeUpdate)
             _roles.RoleUpdateMessage(args.Mind);
     }
@@ -73,59 +56,6 @@ public sealed class JobSystem : SharedJobSystem
 
         _chat.DispatchServerMessage(session, Loc.GetString("job-greet-supervisors-warning", ("jobName", prototype.LocalizedName), ("supervisors", Loc.GetString(prototype.Supervisors))));
     }
-
-    // stalker-changes
-    private void ShowJobRules(EntityUid user, bool silent, JobPrototype prototype)
-    {
-        // Don't spam rules if they are hidden or not configured.
-        if (silent || string.IsNullOrEmpty(prototype.Rules))
-            return;
-
-        if (!_player.TryGetSessionByEntity(user, out var session))
-            return;
-
-        RaiseNetworkEvent(new ShowJobRulesWindowEvent(GetNetEntity(user), prototype.Name, prototype.Rules), session);
-    }
-
-    private void OnShowJobRulesAction(ShowJobRulesActionEvent args)
-    {
-        if (!args.Performer.IsValid())
-            return;
-
-        if (!_player.TryGetSessionByEntity(args.Performer, out var session))
-            return;
-
-        if (!_minds.TryGetMind(session.UserId, out var mindId, out _))
-            return;
-
-        if (!MindTryGetJob(mindId, out var prototype))
-            return;
-
-        ShowJobRules(args.Performer, false, prototype);
-    }
-
-    private void AddJobRulesAction(EntityUid user, JobPrototype prototype)
-    {
-        if (string.IsNullOrEmpty(prototype.Rules))
-            return;
-
-        // Ensure old action is removed when switching roles.
-        RemoveJobRulesAction(user);
-
-        var comp = EnsureComp<JobRulesActionComponent>(user);
-        _actions.AddAction(user, ref comp.ActionEntity, "ActionShowJobRules");
-    }
-
-    private void RemoveJobRulesAction(EntityUid user)
-    {
-        if (!TryComp<JobRulesActionComponent>(user, out var comp) || comp.ActionEntity == null)
-            return;
-
-        _actions.RemoveAction(comp.ActionEntity);
-        RemComp<JobRulesActionComponent>(user);
-    }
-    // stalker-changes-end
-
     public void MindAddJob(EntityUid mindId, string jobPrototypeId)
     {
         if (MindHasJobWithId(mindId, jobPrototypeId))
