9dca4fe6361696f9055e6d5df3deb69f4085f482
CattyLucky <mpalhozaev@gmail.com>
2026-03-27 03:12:31 +0600

    Ошибки

--- Content.Server/_Stalker/PersistentCrafting/PersistentCraftBranchProfile.cs
+++ Content.Server/_Stalker/PersistentCrafting/PersistentCraftBranchProfile.cs
@@ -1,9 +1,6 @@
-using Content.Shared._Stalker.PersistentCrafting;
-
 namespace Content.Server._Stalker.PersistentCrafting;
 
 public sealed class PersistentCraftBranchProfile
 {
     public int AvailablePoints;
-    public int SpentPoints;
 }
